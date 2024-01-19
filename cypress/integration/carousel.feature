dc6c8d1b8523ef6441775ceb032d6c6459a5551e
GermanGameArt <109595561+GermanGameArt@users.noreply.github.com>
2024-01-19 08:42:59 -0500

    Carousel scenarios

--- cypress/integration/carousel.feature
+++ cypress/integration/carousel.feature
@@ -3,4 +3,9 @@ Feature: Testing Carousel
 Scenario: Forward navigation in the carousel
   Given I am on the homepage
   When I click on the next button of the carousel
-  Then I should see the next image in the carousel
+  Then I should see next image in the carousel
+
+Scenario: Backward navigation in the carousel
+  Given I am on the homepage
+  When I click on the previous button of the carousel
+  Then I should see previous image in the carousel
--- cypress/integration/carousel/carouselSteps.js
+++ cypress/integration/carousel/carouselSteps.js
@@ -1,18 +1,30 @@
 import { Given, When, Then } from 'cypress-cucumber-preprocessor/steps';
 
 let previusImage;
+let nextImage;
 
 Given('I am on the homepage', () => {
   cy.visit('/');
   previusImage = cy.get('.carousel-inner .carousel-item.active img').invoke('attr', 'src');
+  nextImage = cy.get('.carousel-inner .carousel-item.active img').invoke('attr', 'src');
 });
 
-When('I click on the next button of the carousel', () => {
+When('I click on the previous button of the carousel', () => {
   cy.get('.carousel-control-prev').click();
 });
 
-Then('I should see the next image in the carousel', () => {
+When('I click on the next button of the carousel', () => {
+  cy.get('.carousel-control-next').click();
+});
+
+Then('I should see previous image in the carousel', () => {
   cy.get('.carousel-inner .carousel-item.active img').invoke('attr', 'src').then((src) => {
     expect(src).not.to.equal(previusImage);
+  });
+});
+
+Then('I should see next image in the carousel', () => {
+  cy.get('.carousel-inner .carousel-item.active img').invoke('attr', 'src').then((src) => {
+    expect(src).not.to.equal(nextImage);
   });
 });
