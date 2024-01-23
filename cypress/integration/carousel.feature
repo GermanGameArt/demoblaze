db0154f18c83c679eb4001f52d9d2635f3bb9102
GermanGameArt <109595561+GermanGameArt@users.noreply.github.com>
2024-01-22 22:49:39 -0500

    third carousel test

--- cypress/integration/carousel.feature
+++ cypress/integration/carousel.feature
@@ -9,3 +9,8 @@ Scenario: Backward navigation in the carousel
   Given I am on the homepage
   When I click on the previous button of the carousel
   Then I should see previous image in the carousel
+
+Scenario: Carousel autoplay
+  Given I am on the homepage
+  When the carousel starts autoplaying
+  Then I should see images changing automatically in the carousel
--- cypress/integration/carousel/carouselSteps.js
+++ cypress/integration/carousel/carouselSteps.js
@@ -2,6 +2,7 @@ import { Given, When, Then } from 'cypress-cucumber-preprocessor/steps';
 
 let previusImage;
 let nextImage;
+let currentImage;
 
 Given('I am on the homepage', () => {
   cy.visit('/');
@@ -17,6 +18,10 @@ When('I click on the next button of the carousel', () => {
   cy.get('.carousel-control-next').click();
 });
 
+When('the carousel starts autoplaying', () => {
+  cy.get('.carousel').trigger('mouseenter'); 
+});
+
 Then('I should see previous image in the carousel', () => {
   cy.get('.carousel-inner .carousel-item.active img').invoke('attr', 'src').then((src) => {
     expect(src).not.to.equal(previusImage);
@@ -26,5 +31,12 @@ Then('I should see previous image in the carousel', () => {
 Then('I should see next image in the carousel', () => {
   cy.get('.carousel-inner .carousel-item.active img').invoke('attr', 'src').then((src) => {
     expect(src).not.to.equal(nextImage);
+  });
+});
+
+Then('I should see images changing automatically in the carousel', () => {
+  cy.wait(5000);
+  cy.get('.carousel-inner .carousel-item.active img').invoke('attr', 'src').then((src) => {
+    expect(src).not.to.equal(currentImage);
   });
 });
